06c3fc45ba1fd9af98957c375c9f98ccecbff7b1
Dabris X <dabris16.x@gmail.com>
2018-09-23 15:16:33 -0400

    in process

--- src/A1.java
+++ src/A1.java
@@ -1,9 +1,17 @@
 
 public class A1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static int i;
+	private static int j;
+	private static int k;
+	private static int[] oddoNums = {-1,-1,-1};
+	public static int[] linearOdd(int x){
+		switch (x) {
+		case 3: oddoNums[2]=1;
+		case 2: oddoNums[1]=1;
+		case 1: oddoNums[0]=1;
+		break;
+		}
+		return oddoNums;
 	}
 
 }
