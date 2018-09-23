9075eed8c2607e05df9e224e4f83906f9b79c3f0
Dabris X <dabris16.x@gmail.com>
2018-09-23 15:34:49 -0400

    linear recursion to be tested

--- src/A1.java
+++ src/A1.java
@@ -1,17 +1,27 @@
 
 public class A1 {
-	private static int i;
-	private static int j;
-	private static int k;
+	
 	private static int[] oddoNums = {-1,-1,-1};
+	private static int i=oddoNums[0];
+	private static int j=oddoNums[1];
+	private static int k=oddoNums[2];
+
 	public static int[] linearOdd(int x){
+		if(x<=0) {
+			return oddoNums;
+		}
 		switch (x) {
 		case 3: oddoNums[2]=1;
 		case 2: oddoNums[1]=1;
 		case 1: oddoNums[0]=1;
-		break;
-		}
 		return oddoNums;
+		}
+			oddoNums=linearOdd(x-1);
+			oddoNums[0]=i+j+k;
+			oddoNums[1]=j+k;
+			oddoNums[2]=j;
+			return oddoNums;
+		
 	}
 
 }
